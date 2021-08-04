7517c468cf7dcfbba5de9bbd0df372d501959240
Edgardo Carreras <edgardo.carreras@galaxydigital.io>
2021-08-03 23:03:04 -0400

    Refactor

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -10,12 +10,11 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] (let [name (:name a)]
-                                 (str name  " eats veggies.")))
-(defmethod diet :carnivore [a] (let [name (:name a)]
-                                 (str name " eats animals.")))
-(defmethod diet :default [a] (let [name (:name a)]
-                               (str "I don't know what " name " eats.")))
+
+
+(defmethod diet :herbivore [a] (str (:name a) " eats veggies."))
+(defmethod diet :carnivore [a] (str (:name a) " eats animals."))
+(defmethod diet :default [a] (str "I don't know what "  (:name a) " eats."))
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
